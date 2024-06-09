0f1bed40112b73dbcb36fd407110cac926d383e0
vamosalaplaya17 <e_putzu@outlook.de>
2024-06-09 13:27:04 +0200

    changed to in-memory database H2

--- src/main/java/org/thws/management/ManagementApplication.java
+++ src/main/java/org/thws/management/ManagementApplication.java
@@ -2,8 +2,10 @@ package org.thws.management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class ManagementApplication {
 
     public static void main(String[] args) {
